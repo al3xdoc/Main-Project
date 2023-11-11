c30e0ceae4925fa3b1a43c4be1e887a4d0adae80
Malak <malakjmammadova@gmail.com>
2023-11-11 12:20:22 +0400

    buttons and s

--- calculator/file.py
+++ calculator/file.py
@@ -2,6 +2,10 @@ import tkinter as tk
 
 WHITE = '#FFFFFF'
 BLACK = '#000000'
+Large_font = ('Arial', 40, 'bold')
+Small_font = ('Arial', 16)
+Digit_font = ('Aial', 24, 'bold')
+
 
 class Calculator:
     def __init__ (self):
@@ -13,14 +17,25 @@ class Calculator:
         self.button_frame = self.create_button_frame()
         self.current_expression_text = "0"
         self.result_expression_text = "0"
+        self.current_label, self.result_label = self.create_labels()
+        self.digits = {7:(1,1), 8:(1,2), 9:(1,3),
+                       4:(2,1), 5:(2,2), 6:(2,3),
+                       1:(3,1), 2:(3,2), 3:(3,3),
+                                0:(4,2), ".":(4,3)} #create a blank method, create buttons (digit)
     def create_display_frame(self):
-        frame = tk.Frame(self.window, height=221,bg='red')
+        frame = tk.Frame(self.window, height=221,bg=BLACK)
         frame.pack(expand=True, fill='both')
         return frame
     def create_button_frame(self):
-        frame = tk.Frame(self.window, height=446,bg='blue')
+        frame = tk.Frame(self.window, height=446,bg=BLACK)
         frame.pack(expand=True, fill='both')
         return frame
+    def create_labels(self):
+        current_label = tk.Label(self.display_frame,text = self.current_expression_text, bg = BLACK, foreground = WHITE, font = Small_font, anchor = tk.E , padx = 24)
+        current_label.pack(expand= True, fill= 'both')
+        result_label = tk.Label(self.display_frame,text = self.result_expression_text, bg = BLACK, foreground = WHITE, font = Large_font, anchor = tk.E , padx = 24)
+        result_label.pack(expand = True, fill = 'both')
+        return current_label, result_label
 
     def run(self):
         self.window.mainloop()

cf85254ae1ce6680c813b5ed86deb15c8a44209f
Malak <malakjmammadova@gmail.com>
2024-02-24 13:45:57 +0400

    added delete, sqrt, square and clear buttons. Also made the claculator more better to look at

--- calculator/file.py
+++ calculator/file.py
@@ -26,6 +26,14 @@ class Calculator:
         self.operations = {"/" : "\u00F7", "*" : "\u00D7" , "-" : "-", "+" : "+" , }
         self.create_operation_buttons()
         self.create_equal_button()
+        self.create_clear_button()
+        self.create_square_button()
+        self.create_sqrt_button()
+        self.create_delete_button()
+        self.button_frame.rowconfigure(0,weight = 1)
+        for x in range(1,5):
+            self.button_frame.rowconfigure(x,weight = 1)
+            self.button_frame.columnconfigure(x, weight = 1)
     def create_display_frame(self):
         frame = tk.Frame(self.window, height=221,bg=BLACK)
         frame.pack(expand=True, fill='both')
@@ -42,23 +50,66 @@ class Calculator:
         return current_label, result_label
     def create_digit_buttons(self):
         for digit, grid_value in self.digits.items():
-            button = tk.Button(self.button_frame, text = str(digit), bg= BLACK, foreground= WHITE, font= Digit_font) 
+            button = tk.Button(self.button_frame, text = str(digit), bg= BLACK, foreground= WHITE, font= Digit_font, command= lambda x = digit : self.add_to_expression(value = x) ) 
             button.grid(row=grid_value[0] , column=grid_value[1], sticky=tk.NSEW)
     def create_operation_buttons(self):
         row = 0
         for operator, symbol in self.operations.items():
-            button = tk.Button(self.button_frame, text = symbol, bg= BLACK, foreground= WHITE, font= Digit_font)
+            button = tk.Button(self.button_frame, text = symbol, bg= BLACK, foreground= WHITE, font= Digit_font, command = lambda x = operator : self.add_operation(operator = x) )
             button.grid(row = row, column = 4, sticky=tk.NSEW)
             row += 1
     def create_equal_button(self):
-        button = tk.Button(self.button_frame, text = "=", bg= BLACK, foreground= WHITE, font= Digit_font)
+        button = tk.Button(self.button_frame, text = "=", bg= BLACK, foreground= WHITE, font= Digit_font, command = self.equal)
         button.grid(row = 4, column = 4, sticky =tk.NSEW )
+    def create_clear_button(self):
+        button = tk.Button(self.button_frame, text = "C", bg = BLACK, foreground = WHITE, font= Digit_font, command = self.clear_calculator)
+        button.grid(row = 0, column = 1, sticky = tk.NSEW ) 
+    def clear_calculator(self):
+         self.result_expression_text = "0"
+         self.current_expression_text = "0"
+         self.update_current_label()
+         self.update_result_label()
+    def create_square_button(self):
+        button = tk.Button(self.button_frame, text = "x\u00b2", bg = BLACK, foreground = WHITE, font= Digit_font, command = self.square)
+        button.grid(row = 0, column = 2, sticky = tk.NSEW ) 
+    def square(self):
+        self.result_expression_text = str(eval(f"{self.result_expression_text}** 2"))
+        self.update_result_label()
+    def create_sqrt_button(self):
+        button = tk.Button(self.button_frame, text = "\u221ax", bg = BLACK, foreground = WHITE, font= Digit_font, command = self.sqrt)
+        button.grid(row = 0, column = 3, sticky = tk.NSEW ) 
+    def sqrt(self):
+        self.result_expression_text = str(eval(f"{self.result_expression_text}** (1/2)"))
+        self.update_result_label()
+    def create_delete_button(self):
+        button = tk.Button(self.button_frame, text = "<=", bg = BLACK, foreground = WHITE, font= Digit_font, command = self.delete)
+        button.grid(row = 4, column = 1, sticky = tk.NSEW ) 
+    def delete(self):
+        self.result_expression_text = self.result_expression_text[:-1]
+        self.update_result_label()
     def add_to_expression(self, value):
-        self.result_expression_text += str(value) 
-        self.update_result_label() #create a method that update current label  
+        if self.result_expression_text == "0":
+            self.result_expression_text = str(value)
+        else:
+            self.result_expression_text += str(value) 
+        self.update_result_label() 
+    def add_operation(self, operator):
+        if self.current_expression_text == "0":
+            self.current_expression_text = self.result_expression_text + operator
+        else:
+            self.current_expression_text += self.result_expression_text + operator
+        self.update_current_label()
+        self.result_expression_text = "0"
+    def equal(self):
+        self.current_expression_text += self.result_expression_text
+        self.result_expression_text = eval(self.current_expression_text)
+        self.result_expression_text = str(self.result_expression_text) 
+        self.update_result_label()
+        self.update_current_label()
     def update_result_label(self):
         self.result_label.config(text = self.result_expression_text)
-
+    def update_current_label(self):
+        self.current_label.config(text = self.current_expression_text)
 
     def run(self):
         self.window.mainloop()
@@ -66,5 +117,4 @@ class Calculator:
 if __name__ == "__main__":
     cal = Calculator()
     cal.run()
-
- 
+       
